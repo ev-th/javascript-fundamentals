40d89efc84907bd95a02fe71176f1dd2f073c991
Evan Thomas <evanthomas0292@gmail.com>
2023-05-23 11:55:03 +0100

    write challenges/02_search_function

---
+++ challenges/02_search_function/searchCandies.test.js
@@ -0,0 +1,19 @@
+const searchCandies = require('./searchCandies')
+
+describe('searchCandies', () => {
+    it('returns Mars and Maltesers when passed Ma and 10', () => {
+        expect(searchCandies('Ma', 10)).toEqual(['Mars', 'Maltesers']);
+    })
+
+    it('returns Mars when passed Ma and 2', () => {
+        expect(searchCandies('Ma', 2)).toEqual(['Mars']);
+    })
+
+    it('returns Skitties, Skittles and Starburst when passed S and 10', () => {
+        expect(searchCandies('S', 10)).toEqual(['Skitties', 'Skittles', 'Starburst']);
+    })
+
+    it('returns Skitties and Skittles when passed S and 4', () => {
+        expect(searchCandies('S', 4)).toEqual(['Skitties', 'Skittles']);
+    })
+})
